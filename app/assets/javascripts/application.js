0dd3f207ce9800cac666196effdce3a4bc3843c3
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-04-20 02:18:33 +1200

    added in javascript flow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
+var ready;
+ready = function() {
+
+	var iframe = $('#vimeo')[0],
+	player = $f(iframe);
+	// When the player is ready, add listeners for pause, finish, and playProgress
+	
+	player.addEvent('ready', function() {
+	  player.addEvent('pause', onHalt);
+	  player.addEvent('finish', onHalt);
+	  player.addEvent('play', onResume);
+	});
+
+	function onResume(id) {
+	  $('.halt').fadeOut();
+	}
+	function onHalt(id) {
+	  $('.halt').fadeIn();
+	}
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);

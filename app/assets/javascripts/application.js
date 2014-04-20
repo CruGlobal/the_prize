e7395ae7fed8cc60e1e436c86497d8931fe69f0f
Andrew Roth <andrewroth@gmail.com>
2014-04-20 01:01:29 -0400

    handle youtube video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,16 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+function onResume(id) {
+  //$('.halt').fadeOut();
+}
+function onHalt(id) {
+  //$('.halt').fadeIn();
+}
+function onFinish(id) {
+  $('.halt').fadeIn();
+}
+
 var ready;
 ready = function() {
 	var iframe = $('#player')[0];
@@ -25,16 +35,46 @@ ready = function() {
     // When the player is ready, add listeners for pause, finish, and playProgress
     player.addEvent('ready', function() {
       player.addEvent('pause', onHalt);
-      player.addEvent('finish', onHalt);
+      player.addEvent('finish', onFinish);
       player.addEvent('play', onResume);
     });
 
-    function onResume(id) {
-      $('.halt').fadeOut();
+  }
+
+  var ytdiv = $('#ytplayer')[0];
+
+  if (ytdiv !== undefined) {
+    var onPlayerStateChange = function (evt) {
+      switch (evt.data) {
+        case YT.PlayerState.ENDED:
+          onFinish();
+          break;
+      }
     }
-    function onHalt(id) {
-      $('.halt').fadeIn();
-    }
+
+    var tag = document.createElement('script');
+    var title = document.title;
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    window.onYouTubeIframeAPIReady = function () {
+      $('body').addClass('video');
+      YTplayer = new YT.Player('ytplayer', {
+        videoId: 'bM5aISQiHOI',
+        playerVars: {
+          autoplay: 1,
+          modestbranding: 1,
+          rel: 0,
+          showinfo: 0,
+          iv_load_policy: 3,
+          html5: 1,
+        },
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    };
   }
 };
 

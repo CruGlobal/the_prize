10dd97768af58ddfab33507e66555e0c172c9d31
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-04-20 17:10:27 +1200

    added language selector

---
+++ spec/factories/languages.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :language do
+    name "MyString"
+    country "MyString"
+    code "MyString"
+    locale "MyString"
+  end
+end

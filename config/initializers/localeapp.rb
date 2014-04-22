d2e96061072ed51d65075ee80e068e3266cfd6cd
Andrew Roth <andrewroth@gmail.com>
2014-04-22 11:38:49 -0400

    switch the homepage title back to what it was

--- config/initializers/localeapp.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  require 'localeapp/rails'
-
-  Localeapp.configure do |config|
-    config.api_key = ENV['localeapp_key']
-    config.polling_environments = [:development]
-    config.sending_environments = [:development]
-  end
-end
